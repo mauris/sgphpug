9fd448cd23626beacade3cc9a4eb144226995d30
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-10 14:57:56 +0800

    updated event loading from fb

--- app/src/SGPHPUG/Event/Event.php
+++ app/src/SGPHPUG/Event/Event.php
@@ -4,6 +4,7 @@ namespace SGPHPUG\Event;
 
 use Packfire\FuelBlade\ConsumerInterface;
 use Packfire\DateTime\TimeSpan;
+use Packfire\DateTime\DateTime;
 
 class Event implements ConsumerInterface
 {
@@ -28,9 +29,30 @@ class Event implements ConsumerInterface
         $files = glob(__DIR__ . '/*.json');
         $events = array();
         foreach ($files as $file) {
-            $events[] = json_decode(file_get_contents($file), true);
+            $event = json_decode(file_get_contents($file), true);
+            if ($token && $event['fb_event']) {
+                $fb_url = 'https://graph.facebook.com/' . $event['fb_event'] .'?access_token=' . $token;
+                $fbevent = json_decode(file_get_contents($fb_url), true);
+
+                $start = DateTime::fromString($fbevent['start_time']);
+                $start->timezone(8);
+                $end = DateTime::fromString($fbevent['end_time']);
+                $end->timezone(8);
+
+                $fbevent['start_time'] = $start->format('d M Y, l, g:i a');
+                $isSameDate = self::isSameDate($start->toTimestamp(), $end->toTimestamp());
+                $fbevent['end_time'] = $end->format($isSameDate ? 'g:i a' : 'd M Y, l, g:i a');
+
+                $event['fb_event'] = $fbevent;
+            }
+            $events[] = $event;
         }
         return $events;
+    }
+
+    protected static function isSameDate($date1, $date2)
+    {
+        return gmdate('dMY', $date1) == gmdate('dMY', $date2);
     }
 
     public function __invoke($container)
