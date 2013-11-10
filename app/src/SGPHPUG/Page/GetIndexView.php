d6220ba21c75f30db6f8f8236d75e49812f800c4
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-10 12:56:46 +0800

    updated event loading on page

--- app/src/SGPHPUG/Page/GetIndexView.php
+++ app/src/SGPHPUG/Page/GetIndexView.php
@@ -3,11 +3,13 @@
 namespace SGPHPUG\Page;
 
 use Packfire\Application\Pack\View;
+use SGPHPUG\Event\Event;
 
 class GetIndexView extends View
 {
     protected function create()
     {
-        
+        $provider = new Event();
+        $this->define('events', $provider->loadAll());
     }
 }
