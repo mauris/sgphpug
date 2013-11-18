ed1834f9a86cb1368e445537c51a06ab60c71013
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-18 20:20:42 +0800

    updated get() in Event

--- app/src/SGPHPUG/Event/Event.php
+++ app/src/SGPHPUG/Event/Event.php
@@ -15,23 +15,17 @@ class Event implements ConsumerInterface
 
     protected $token;
 
+    public function get($eventId)
+    {
+        $event = array();
+        if (file_exists(__DIR__ . '/' . $eventId . '.json')) {
+            $event = $this->loadFile(__DIR__ . '/' . $eventId . '.json');
+        }
+        return $event;
+    }
+
     public function loadAll()
     {
-        $cache = $this->cache;
-        $token = null;
-        if ($cache->check('fb_access_token')) {
-            $token = $cache->get('fb_access_token');
-        }
-        if (!$token) {
-            $details = array(
-                'appId'  => '1374863032759788',
-                'secret' => '3834111b049efc7d7290d18fa184b053',
-            );
-            $token = str_replace('access_token=', '', file_get_contents(sprintf('https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&grant_type=client_credentials', $details['appId'], $details['secret'])));
-            $cache->set('fb_access_token', $token, new TimeSpan(216000));
-        }
-        $this->token = $token;
-
         $files = array_reverse(glob(__DIR__ . '/*.json'));
         $events = array();
         foreach ($files as $file) {
@@ -114,5 +108,19 @@ class Event implements ConsumerInterface
     {
         $this->octurlpus = new Octurlpus();
         $this->cache = $container['cache'];
+        $cache = $this->cache;
+        $token = null;
+        if ($cache->check('fb_access_token')) {
+            $token = $cache->get('fb_access_token');
+        }
+        if (!$token) {
+            $details = array(
+                'appId'  => '1374863032759788',
+                'secret' => '3834111b049efc7d7290d18fa184b053',
+            );
+            $token = str_replace('access_token=', '', file_get_contents(sprintf('https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&grant_type=client_credentials', $details['appId'], $details['secret'])));
+            $cache->set('fb_access_token', $token, new TimeSpan(216000));
+        }
+        $this->token = $token;
     }
 }
